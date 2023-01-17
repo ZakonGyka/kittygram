7a0cacdbe01299b54dda66e9a8f3283e62c215ad
zakongyka <zakongyka@yandex.ru>
2023-01-17 01:01:07 +0100

    the project is being done

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,9 +1,63 @@
 from rest_framework import serializers
+import datetime
+import webcolors
 
-from .models import Cat
+from .models import Cat, Owner, Achievement, AchievementCat, CHOICES
+
+
+class Hex2NameColor(serializers.Field):
+    def to_representation(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        return data
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
+    # owner = serializers.StringRelatedField(read_only=True)
+    achievements = AchievementSerializer(required=False, many=True)
+    age = serializers.SerializerMethodField()
+    color = serializers.ChoiceField(choices=CHOICES)
+
+    def create(self, validated_data):
+        if 'achievements' not in self.initial_data:
+            cat = Cat.objects.create(**validated_data)
+            return cat
+
+        achievements = validated_data.pop('achievements')
+        cat = Cat.objects.create(**validated_data)
+        for achievement in achievements:
+            current_achievement, status = Achievement.objects.get_or_create(**achievement)
+            AchievementCat.objects.create(achievement=current_achievement,
+                                          cat=cat)
+        return cat
+
     class Meta:
         model = Cat
-        fields = '__all__'
+        fields = ('id', 'name', 'color', 'birth_year', 'owner',
+                  'achievements', 'age')
+
+    def get_age(self, obj):
+        return datetime.datetime.now().year - obj.birth_year
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Owner
+        fields = ('id', 'first_name', 'last_name', 'cats')
+
+
--- cats/views.py
+++ cats/views.py
@@ -1,15 +1,56 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import viewsets
 
-from .models import Cat
-from .serializers import CatSerializer
+from .models import Cat, Owner
+from .serializers import CatSerializer, OwnerSerializer
 
 
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+
+# class CatList(generics.ListCreateAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
+
+# class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
+
+'''
+class APICAT(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CatSerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class APICatDetail(APIView):
+
+    def patch(self, request, id):
+        cat = Cat.objects.filter(id=id)
+        serializer = CatSerializer(cat)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Функция cat_list работает при помощи декоратора @api_view
 @api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
+        serializer = CatSerializer(data=request.data, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -17,3 +58,4 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+'''
